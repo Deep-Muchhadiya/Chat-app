c79e52c68274e246f0c154989a3b02072cd20582
Deep <deepmuchhadiya29@gmail.com>
2026-01-05 23:20:47 +0530

    code uploaded

---
+++ nodeServer/server.js
@@ -0,0 +1,34 @@
+
+const express = require("express"); 
+const http = require("http"); 
+const { Server } = require("socket.io"); 
+const cors = require("cors"); 
+const app = express();
+app.use(cors({ origin: "http://127.0.0.1:5500" })); 
+const server = http.createServer(app); 
+const io = new Server(server, { cors: { origin: "http://127.0.0.1:5500", methods: ["GET", "POST"] } });
+
+const users = {};
+
+io.on('connection', socket => {
+    socket.on('new-user-joined', name => {
+        console.log("user joined", name);
+        users[socket.id] = name;
+        socket.broadcast.emit('user-joined', name);
+    })
+
+    socket.on('send', message => {
+        socket.broadcast.emit('receive', {message: message, name: users[socket.id]})
+    })
+
+    socket.on('disconnect', ()=> {
+        if(users[socket.id] == null || users[socket.id] == undefined){
+            return;
+        }
+        socket.broadcast.emit('left', users[socket.id]);
+        console.log(`${users[socket.id]} left`);
+        delete users[socket.id];
+    })
+})
+
+server.listen(3000);
